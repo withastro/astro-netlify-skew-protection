ea98dc374ceba91af55d9484e6d8c678d848b673
Matthew Phillips <matthew@skypack.dev>
2025-10-27 13:09:21 -0400

    fix the font build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
@@ -11,12 +11,10 @@ export default defineConfig({
     inlineStylesheets: 'never'
   },
   experimental: {
-    fonts: [
-      {
-        name: 'Inter',
-        src: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap',
-        preloadFontSize: 'all'
-      }
-    ]
+    fonts: [{
+      provider: fontProviders.google(),
+      name: 'Inter',
+      cssVariable: '--font-inter'
+    }]
   }
 });
